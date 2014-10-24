926dc4050253f2afca26826d4b5fe84e01b46a27
cvp21594 <cvp45@mail.umkc.edu>
2014-10-24 13:03:05 -0500

    Still need error help ASAP
    Here is code that should work for booleans and arithmetic, still needs testing but for now we need to solve linking errors.  I am getting unresolved external symbol errors in this code and I cannot find out why.  After that all this needs is some quick testing til it's polished and works!

--- CS303_Project2/CS303_Project2/Evaluator.cpp
+++ CS303_Project2/CS303_Project2/Evaluator.cpp
@@ -10,18 +10,27 @@
 //October 22nd  right now this does not handle boolean expressions, will be made to
 
 
-int precedence(char text)
+int precedence(string text)
 {
-	if (text == '(')
+	if (text == "LPAREN")
 		return 0;
 
-	if (text == '+' || text == '-')
+	else if (text == "EQ" || text == "NE")
+		return 1;
+
+	else if (text == "AND" || text == "OR")
+		return 2;
+
+	else if (text == "GT" || text == "GE" || text == "LT" || text == "LE")
+		return 3;
+
+	else if (text == "PLUS" || text == "MINUS")
 		return 4;
 
-	else if (text == '*' || text == '/' || text == '%')
+	else if (text == "MULT" || text == "DIV" || text == "MOD")
 		return 5;
 
-	else if (text == '^')
+	else if (text == "POWER")
 		return 6;
 
 	else
@@ -30,51 +39,74 @@ int precedence(char text)
 }
 
 
-bool isOperand(char text)
+
+bool isOperator(string text)
 {
-	return(text == '0' || text == '1' || text == '2' || text == '3' || text == '4' || text == '5' || text == '6' || text == '7' || text == '8' || text == '9');
+	return(text == "PLUS" || text == "MINUS" || text == "MULT" || text == "MOD" || text == "DIV" || text == "POWER"
+		|| text == "GT" || text == "LT" || text == "GE" || text == "LE" || text == "NE" || text == "EQ" ||text == "OR" || text == "AND");
 }
 
-bool isOperator(char text)
+bool isOpen(string text)
 {
-	return(text == '+' || text == '-' || text == '*' || text == '%' || text == '/' || text == '^');
+	return (text == "LPAREN");
 }
 
-bool isOpen(char text)
+bool isClose(string text)
 {
-	return (text == '(');
+	return (text == "RPAREN");
 }
 
-bool isClose(char text)
+int process(int lhs, int rhs, string oper)
 {
-	return (text == ')');
-}
-
-int process(int lhs, int rhs, char oper)
-{
-	if (oper == '+') {
+	if (oper == "PLUS") {
 		return lhs + rhs;
 	}
-	if (oper == '-') {
+	if (oper == "MINUS") {
 		return lhs - rhs;
 	}
-	if (oper == '/') {
+	if (oper == "DIV") {
 		return lhs / rhs;
 	}
-	if (oper == '*') {
+	if (oper == "MULT") {
 		return lhs*rhs;
 	}
-	if (oper == '%') {
+	if (oper == "MOD") {
 		return lhs%rhs;
 	}
-	if (oper == '^')
+	if (oper == "POWER")
 	{
 		for (int i = 1; i < rhs; i++)
 			lhs = lhs*lhs;
-
 		return lhs;
 	}
+	if (oper == "GT") {
+		return(lhs > rhs);
+	}
+	if (oper == "LT") {
+		return(lhs < rhs);
+	}
+	if (oper == "LE") {
+		return(lhs <= rhs);
+	}
+	if (oper == "GE") {
+		return(lhs >= rhs);
+	}
+	if (oper == "EQ") {
+		return(lhs == rhs);
+	}
+	if (oper == "NE") {
+		return(lhs != rhs);
+	}
+	if (oper == "AND") {
+		return(lhs && rhs);
+	}
+	if (oper == "OR") {
+		return(lhs || rhs);
+	}
+}
 
+bool isNot(string text) {
+	return text == "NOT";
 }
 
 
@@ -82,9 +114,9 @@ int evaluate_expression(string& input)
 {
 	SyntaxChecker check;
 	int rhs, lhs, result;
-	char oper;
+	string oper;
 	stack<int> operands;
-	stack<syntax_status> operators;
+	stack<string> operators;
 
 	list<exprToken> expression;
 	
@@ -98,20 +130,25 @@ int evaluate_expression(string& input)
 	for (list<exprToken>::iterator itr = expression.begin(); itr != expression.end(); ++itr) 
 	{
 		//If it's a number push it on the operand stack
-		if (isOperand(itr->number))
+		if (itr->isANumber)
 		{
-			operands.push(itr->token);
+			operands.push(itr->number);
+			if (isNot(operators.top())) {
+				result = !operands.top();
+				operands.pop();
+				operands.push(result);
+			}
 		}
 		//If it's an operator
-		else if (isOperator(itr->token))
+		else if (isOperator(itr->toString()))
 		{
 			//if there are none, push it onto the stack
 			if (operators.empty())
 			{
-				operators.push(itr->token);
+				operators.push(itr->toString());
 			}
 			//if it's precedence is lower or equal to what's on top, process the last one
-			else if (precedence(itr->token) <= precedence(operators.top()))
+			else if (precedence(itr->toString()) <= precedence(operators.top()))
 			{
 				rhs = operands.top();
 				operands.pop();
@@ -120,21 +157,21 @@ int evaluate_expression(string& input)
 				operators.pop();
 				result = process(lhs, rhs, oper);
 				operands.push(result);
-				operators.push(itr->token);
+				operators.push(itr->toString());
 			}
 			//if it's of higher precedence, push it on to be processed later
-			else if (precedence(itr->token) > precedence(operators.top()))
+			else if (precedence(itr->toString()) > precedence(operators.top()))
 			{
-				operators.push(itr->token);
+				operators.push(itr->toString());
 			}
 		}
 		//it's an opening parenthesis, put it in operator stack
-		else if (isOpen(itr->token))
+		else if (isOpen(itr->toString()))
 		{
-			operators.push(itr->token);
+			operators.push(itr->toString());
 		}
 		//It's a closing parenthesis, process until the last opening parenthesis
-		else if (isClose(itr->token))
+		else if (isClose(itr->toString()))
 		{
 			while (!isOpen(operators.top()))
 			{
@@ -147,9 +184,17 @@ int evaluate_expression(string& input)
 				operands.push(process(lhs, rhs, oper));
 			}
 			operators.pop(); //dump the last opening parenthesis, we're done with it
+			if (isNot(itr->toString())) {
+				//DO THE NOT THING
+			}
+		}
+		else if (isNot(itr->toString()))
+		{
+			operators.push(itr->toString());
 		}
 
 	}
+
 	while(!operators.empty())
 	{
 		rhs = operands.top();

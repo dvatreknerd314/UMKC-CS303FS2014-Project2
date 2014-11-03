d7c160c192a4077691b2ba3f1e3cee8661596277
dvatreknerd314 <dvatreknerd314@gmail.com>
2014-11-03 15:09:13 -0600

    Chris's code

--- source.cpp
+++ source.cpp
@@ -1,32 +1,19 @@
+//Chris Yuhas
+//CS 303
+//Checks to see that wrong input generates an error for the syntax checker
+#include "Constants.h"
 #include "syntax_checker.h"
-#include "Evaluator.h"
 #include <iostream>
-#include <fstream>
-#include <list>
 using namespace std;
-/*
-int main() {
-	string the_input;
-	ifstream test_file("test_input.txt");
-	SyntaxChecker syntaxChecker;
-	list<exprToken> theList;
-	while (!test_file.eof()) {
-		getline(test_file, the_input);
-		cout << "Evaluating " << the_input << " : ";
-		if (!syntaxChecker.syntax_check(the_input, theList))
-		{
-			for (list<exprToken>::iterator i = theList.begin(); i != theList.end(); i++)
-			{
-				cout << (*i).toString() << " ";
-			}
-			cout << endl;
-			cout << "Evaluates to: " << evaluate_expression(the_input);
-			cout << endl;
-		}
-		theList.clear();
-	}
-	test_file.close();
-	return 0;
-}
 
-*/
+int main()
+{
+	SyntaxChecker ans;
+	string num;
+	cout << "Enter equeation: ";
+	cin >> num;
+	ans.syntax_check(num);
+	system("pause");
+	return 0;
+
+}
